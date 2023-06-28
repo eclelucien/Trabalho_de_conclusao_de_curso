0f073549fb849e85ad3166a8093ae39c7775567f
Eclesiaste Lucien <eclesiastelucien@gmail.com>
2023-06-27 21:01:02 -0300

    Initial commit

---
+++ back-end/lucienstore/src/main/java/com/eclesiastelucien/com/lucienstore/LucienstoreApplication.java
@@ -0,0 +1,13 @@
+package com.eclesiastelucien.com.lucienstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucienstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucienstoreApplication.class, args);
+	}
+
+}
