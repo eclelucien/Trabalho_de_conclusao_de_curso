11a895fd42a1cada767f65afd07ca9b2316853fd
Eclesiaste Lucien <eclesiastelucien@gmail.com>
2023-07-17 22:35:00 -0300

    add categories list in the home page

--- front-end/lucienstore/src/App.js
+++ front-end/lucienstore/src/App.js
@@ -27,9 +27,7 @@ function App() {
       <div className="welcome-container">
         <h1>Welcome to Lucien Store</h1>
         <p>
-          Explore our wide range of products and find great deals.
-          <br />
-          Choose a category:
+          Explore our wide range of products and find great deals...
         </p>
         <ul className="categories-list">
           {categories.map((category, index) => (
