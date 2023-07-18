e20d863e12b63dd4f8bfe1eb273ae9eebd6512ac
Eclesiaste Lucien <eclesiastelucien@gmail.com>
2023-07-17 22:04:11 -0300

    List card of products

--- front-end/lucienstore/src/App.js
+++ front-end/lucienstore/src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import StoreAppBar from './StoreAppBar';
+import ProductCard from './component/ProductCard';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.log('Error fetching products:', error);
+    }
+  };
+
   return (
     <div className="App">
       <StoreAppBar />
       <h1>Welcome to Lucien Store!</h1>
       <p>Explore our wide range of products and find great deals.</p>
+      <div className="product-list">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ front-end/lucienstore/src/component/ProductCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ProductCard.css';
+
+
+const ProductCard = ({ product }) => {
+  return (
+    <div className="product-card">
+      <img src={product.thumbnailUrl} alt={product.title} />
+      <h3>{product.title}</h3>
+      <p>Album ID: {product.albumId}</p>
+      <p>ID: {product.id}</p>
+      <a href={product.url}>View Product</a>
+    </div>
+  );
+};
+
+
+export default ProductCard;
