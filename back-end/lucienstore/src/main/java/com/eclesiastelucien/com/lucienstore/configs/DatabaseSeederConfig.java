ff7764a7a1b1078161c360b5414134c6a6479dfa
Eclesiaste Lucien <eclesiastelucien@gmail.com>
2023-07-25 21:48:32 -0300

    chore: insert users to database initiating project

--- back-end/lucienstore/src/main/java/com/eclesiastelucien/com/lucienstore/configs/DatabaseSeederConfig.java
+++ back-end/lucienstore/src/main/java/com/eclesiastelucien/com/lucienstore/configs/DatabaseSeederConfig.java
@@ -1,8 +1,15 @@
 package com.eclesiastelucien.com.lucienstore.configs;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import com.eclesiastelucien.com.lucienstore.models.User;
+import com.eclesiastelucien.com.lucienstore.repositories.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -10,8 +17,26 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class DatabaseSeederConfig implements CommandLineRunner {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public void run(String... args) throws Exception {
-
+        List<User> users = userRepository.findAll();
+        if(users.isEmpty()) {
+            for (int i = 0; i < 5; i++) {
+                User user = new User();
+                user.setName("Teste 1");
+                user.setEmail("emailteste@gmail.com");
+                user.setPhoneNumber("123456");
+                if(i > 2){
+                    user.setActive(true);
+                }else{
+                    user.setActive(false);
+                }
+                users.add(user);
+            }
+            userRepository.saveAll(users);
+        }
     }
 }
