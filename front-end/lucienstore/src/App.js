ba4adf1d7c12d3ca1a147228596bee5deb90bb39
eclelucien <eclesiastelucien@gmail.com>
2023-11-20 00:02:06 -0300

    chore: correcting import of components

--- front-end/lucienstore/src/App.js
+++ front-end/lucienstore/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import StoreAppBar from './component/StoreAppBar';
-import ProductCard from './component/ProductCard';
-import OptionsList from './component/OptionsList'; 
+import StoreAppBar from './components/StoreAppBar';
+import ProductCard from './components/ProductCard';
+import OptionsList from './components/OptionsList';
 import './App.css';
-import { Carousel } from 'react-responsive-carousel'; 
+import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 function App() {
@@ -40,18 +40,18 @@ function App() {
       </div>
       <div>
         <Carousel showArrows={true} showThumbs={false} interval={3000}>
-            <div>
-              <img src="https://img.ltwebstatic.com/images3_ccc/2023/07/17/16895783130a5b4a7ee74d58ae920e7a8a8f80c6d7_thumbnail_2000x.webp" alt="ImageSlide 2" />
-            </div>
-            <div>
-              <img src="https://img.ltwebstatic.com/images3_ccc/2023/07/10/1688960268466f26d41b9e612de630272752d840d2_thumbnail_2000x.webp" alt="ImageSlide 1" />
-            </div>
-            <div>
-              <img src="https://img.ltwebstatic.com/images3_ccc/2023/07/17/16895783130a5b4a7ee74d58ae920e7a8a8f80c6d7_thumbnail_2000x.webp" alt="ImageSlide 2" />
-            </div>
-          </Carousel>
+          <div>
+            <img src="https://img.ltwebstatic.com/images3_ccc/2023/07/17/16895783130a5b4a7ee74d58ae920e7a8a8f80c6d7_thumbnail_2000x.webp" alt="ImageSlide 2" />
+          </div>
+          <div>
+            <img src="https://img.ltwebstatic.com/images3_ccc/2023/07/10/1688960268466f26d41b9e612de630272752d840d2_thumbnail_2000x.webp" alt="ImageSlide 1" />
+          </div>
+          <div>
+            <img src="https://img.ltwebstatic.com/images3_ccc/2023/07/17/16895783130a5b4a7ee74d58ae920e7a8a8f80c6d7_thumbnail_2000x.webp" alt="ImageSlide 2" />
+          </div>
+        </Carousel>
       </div>
-      <br/>
+      <br />
       <div className="flex-container">
         <OptionsList />
         <div className="product-list">
