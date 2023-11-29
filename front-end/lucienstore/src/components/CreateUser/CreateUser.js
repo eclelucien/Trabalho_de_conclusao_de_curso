14cc439837845f131a977881a579e4fddb36595a
eclelucien <eclesiastelucien@gmail.com>
2023-11-29 20:23:53 -0300

    chore: register screen

--- front-end/lucienstore/src/components/CreateUser/CreateUser.js
+++ front-end/lucienstore/src/components/CreateUser/CreateUser.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { useAuth } from '../../context/AuthContext'
+import { useAuth } from '../../context/AuthContext';
+import '../Login/Login.css';
+
 
 const CreateUser = () => {
     const { login } = useAuth();
@@ -8,6 +10,13 @@ const CreateUser = () => {
         email: '',
         password: '',
     });
+    const [errorMessages, setErrorMessages] = useState({});
+    const [isSubmitted, setIsSubmitted] = useState(false);
+
+    const renderErrorMessage = (name) =>
+        name === errorMessages.name && (
+            <div className="error">{errorMessages.message}</div>
+        );
 
     const handleChange = (e) => {
         setFormData({
@@ -24,27 +33,39 @@ const CreateUser = () => {
         login(userData);
     };
 
-    return (
-        <div>
-            <h2>Create User</h2>
+    const renderForm = (
+        <div className="form">
             <form onSubmit={handleSubmit}>
-                <label>
-                    Username:
-                    <input type="text" name="username" value={formData.username} onChange={handleChange} />
-                </label>
-                <br />
-                <label>
-                    Email:
-                    <input type="email" name="email" value={formData.email} onChange={handleChange} />
-                </label>
-                <br />
-                <label>
-                    Password:
-                    <input type="password" name="password" value={formData.password} onChange={handleChange} />
-                </label>
-                <br />
-                <button type="submit">Create User</button>
+                <div className="input-container">
+                    <label>Username </label>
+                    <input type="text" name="uname" required />
+                    {renderErrorMessage("uname")}
+                </div>
+                <div className="input-container">
+                    <label>Email </label>
+                    <input type="email" name="email" required />
+                    {renderErrorMessage("email")}
+                </div>
+                <div className="input-container">
+                    <label>Password </label>
+                    <input type="password" name="pass" required />
+                    {renderErrorMessage("pass")}
+                </div>
+                <div className="button-container">
+                    <input type="submit" />
+                </div>
             </form>
+
+
+        </div>
+    );
+
+    return (
+        <div className="app">
+            <div className="login-form">
+                <div className="title">Sign In</div>
+                {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+            </div>
         </div>
     );
 };
