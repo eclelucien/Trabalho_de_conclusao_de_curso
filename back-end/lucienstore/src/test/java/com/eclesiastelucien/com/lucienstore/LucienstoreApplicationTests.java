136d6ce72a996ef3407fdd7cdaa50463d80cd8bd
eclelucien <eclesiastelucien@gmail.com>
2023-12-04 17:19:22 -0300

    chore: remove test

--- back-end/lucienstore/src/test/java/com/eclesiastelucien/com/lucienstore/LucienstoreApplicationTests.java
+++ back-end/lucienstore/src/test/java/com/eclesiastelucien/com/lucienstore/LucienstoreApplicationTests.java
@@ -3,11 +3,11 @@ package com.eclesiastelucien.com.lucienstore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LucienstoreApplicationTests {
+// @SpringBootTest
+// class LucienstoreApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// @Test
+// void contextLoads() {
+// }
 
-}
+// }
